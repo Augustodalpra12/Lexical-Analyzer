4b60a90191beebb205be57efb82e262e5c4cd41c
Dal <augusto_dalpra@hotmail.com>
2024-12-15 15:18:23 -0300

    thinking

---
+++ semantic/main.cpp
@@ -0,0 +1,6 @@
+// pelo que estou entendendo
+// precisa puxar a gramatica do syntatic (grammar.cpp)
+
+// cada produção na gramatica deve conter regras semanticas que manipulam esses atributos (ver se vão ser herdados ou sitentizado )
+
+// criar uma tabela de dados com nome e tipo (nos so temos um escopo ent ta suave)
